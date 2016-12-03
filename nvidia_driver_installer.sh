55b15a75eee22acc7d6e32031bb9d5ad66806af0
sasukeh(Kyohei Moriyama) <flogofrein@gmail.com>
2016-12-04 01:25:38 +0900

    add install cuda procedure

--- nvidia_driver_installer.sh
+++ nvidia_driver_installer.sh
@@ -1,5 +1,24 @@
+# author: sasukeh
+# e-mail: flogofrein@gmail.com
+# USAGE:
+# ./<filename> # this script must have executing rights.
+
+# install packages
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update
 sudo apt-get install nvidia-367
 sudo apt-get install mesa-common-dev
 sudo apt-get install freeglut3-dev
+
+# wait
+sleep 5
+
+# get cuda packages
+wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_linux-run
+
+# register path and lib path and cuda home to bashrc file
+echo "export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc
+echo "export CUDA_HOME=/usr/local/cuda" >> ~/.bashrc
+
+ 
